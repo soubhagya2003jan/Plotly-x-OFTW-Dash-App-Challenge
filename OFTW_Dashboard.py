315b773e04830410b49b9bd0274cf68cf6e0ea60
Soubhagya Swain <119324713+soubhagya2003jan@users.noreply.github.com>
2025-04-14 13:02:00 +0530

    Update OFTW_Dashboard.py

--- OFTW_Dashboard.py
+++ OFTW_Dashboard.py
@@ -2820,4 +2820,4 @@ def analyze_dashboard_metrics(question, df_payments, df_pledges, fiscal_year):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
